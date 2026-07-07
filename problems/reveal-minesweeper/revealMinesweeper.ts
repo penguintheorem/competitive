85a1f532aa48458637ad87d1fe2b419e2b9e2b64
penguintheorem <attiliourb@gmail.com>
2026-07-08 00:26:55 +0200

    fix revealMinesweeper

--- problems/reveal-minesweeper/revealMinesweeper.ts
+++ problems/reveal-minesweeper/revealMinesweeper.ts
@@ -5,6 +5,7 @@ const isExistingCell = (
 ): boolean =>
   row >= 0 && row < board.length && column >= 0 && column < board[0].length
 
+// O(1)
 const getNumberOfMinesAroundCell = (
   board: string[][],
   row: number,
@@ -30,36 +31,45 @@ const getNumberOfMinesAroundCell = (
   return numberOfMines
 }
 
+/**
+ *
+ * |board| = N x M
+ * Time complexity: O(N x M)
+ * Space complexity: O(N x M) (Recursion depth)
+ *
+ *
+ * @param board
+ * @param row
+ * @param column
+ * @returns
+ */
 export function revealMinesweeper(
   board: string[][],
   row: number,
   column: number
 ) {
-  console.log(`START [ROW=${row};COLUMN=${column}]`)
   if (!board.length || !board[0]?.length) {
-    console.log(`EMPTY`)
     return board
   }
-
   // you try to press on an unexisting cell
   if (!isExistingCell(board, row, column)) {
-    console.log(`UNEXISTING CELL`)
+    return board
+  }
+  if (board[row][column] === 'M') {
+    board[row][column] = 'X'
     return board
   }
 
-  if (board[row][column] === 'M') {
-    console.log(`MINE FOUND`)
-    board[row][column] = 'X'
+  const selectedCellAsNumber = Number(board[row][column])
+  if (selectedCellAsNumber >= 0 && selectedCellAsNumber <= 8) {
     return board
   }
 
   // I assume board[row][column] === 'H'
   const numberOfMines = getNumberOfMinesAroundCell(board, row, column)
-  console.log(`number of mines=${numberOfMines}`)
   board[row][column] = `${numberOfMines}`
 
   if (numberOfMines === 0) {
-    console.log(`look around`)
     revealMinesweeper(board, row - 1, column - 1)
     revealMinesweeper(board, row - 1, column)
     revealMinesweeper(board, row - 1, column + 1)
@@ -70,6 +80,5 @@ export function revealMinesweeper(
     revealMinesweeper(board, row + 1, column + 1)
   }
 
-  console.log(`END [ROW=${row};COLUMN=${column}]`)
   return board
 }
